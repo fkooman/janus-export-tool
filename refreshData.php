33b358b8336fc147519771c019c9ffca5cf0641f
François Kooman <fkooman@tuxed.net>
2013-05-24 14:08:27 +0200

    update script

--- refreshData.php
+++ refreshData.php
@@ -1,3 +1,3 @@
 <?php
-system("/usr/bin/php exportJanus.php");
-system("/usr/bin/php entityLogToHtml.php");
+system("/usr/bin/php " . __DIR__ . DIRECTORY_SEPARATOR . "exportJanus.php");
+system("/usr/bin/php " . __DIR__ . DIRECTORY_SEPARATOR . "entityLogToHtml.php");
