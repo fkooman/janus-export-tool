03b4472db3066b30ccb947a80e78008403e5ccd5
François Kooman <fkooman@tuxed.net>
2013-07-25 14:50:14 +0200

    add parseIdPMetadata script, update composer dependencies, add composer.lock file

---
+++ parseIdPMetadata.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once 'MetadataParser.php';
+
+if(3 > $argc) {
+    die("Syntax: <metadata URL> <entity ID>" . PHP_EOL);
+}
+
+var_export(MetadataParser::idp($argv[1], $argv[2]));
